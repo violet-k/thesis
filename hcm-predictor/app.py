ded5113c9d59ea927856bd69749b030616391abf
Violet <52976585+violet-k@users.noreply.github.com>
2023-04-21 16:13:14 +0300

    Add files via upload

--- hcm-predictor/app.py
+++ hcm-predictor/app.py
@@ -1,59 +1,34 @@
-from neo4j import GraphDatabase
-import logging
-from neo4j.exceptions import ServiceUnavailable
-import os
-from dotenv import load_dotenv
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from xgboost import XGBClassifier
 
+# Load the data
+df = pd.read_csv('results_prob_2.csv')
 
-load_dotenv()
+# Extract the required columns
+df = df[['Sex', 'Has_HCM', 'Sire_has_HCM', 'Dam_has_HCM', 'Probability']]
 
+# # Load the data
+# df = pd.read_csv('results.csv')
 
-class App:
+# # Extract the required columns
+# df = df[['Sex', 'Has_HCM', 'Sire_has_HCM', 'Dam_has_HCM']]
 
-    def __init__(self, uri, user, password):
-        self.driver = GraphDatabase.driver(uri, auth=(user, password))
+# Convert categorical variables to one-hot encoded variables
+encoder = OneHotEncoder(sparse=False)
+df_encoded = pd.DataFrame(encoder.fit_transform(df[['Sex']]))
+df_encoded.columns = encoder.get_feature_names(['Sex'])
+df = pd.concat([df_encoded, df.drop('Sex', axis=1)], axis=1)
 
-    def close(self):
-        self.driver.close()
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(df.drop('Has_HCM', axis=1), df['Has_HCM'], test_size=0.2, random_state=42)
 
-    def test(self):
-        with self.driver.session(database="neo4j") as session:
-            result = session.execute_write(
-                self.get_paths_with_2_plus_HCM
-            )
-            for row in result:
-                print(row)
+# Train the model
+model = XGBClassifier()
+model.fit(X_train, y_train)
 
-    @staticmethod
-    def get_paths_with_2_plus_HCM(tx):
-        query = """
-            MATCH (leaf:Cat)
-            WHERE NOT (leaf)-[:SIRE|DAM]->()
-            CALL apoc.path.spanningTree(leaf, {relationshipFilter: "<SIRE|<DAM", minLevel: 1})
-            YIELD path
-            WITH path, length(path) as path_length, [node in nodes(path) WHERE node.HCM = "HCM"] as hcm_nodes
-            WHERE size(hcm_nodes) > 1
-            WITH path, path_length
-            ORDER BY path_length DESC
-            UNWIND nodes(path) AS n
-            OPTIONAL MATCH (n)<-[:SIRE]-(sire) 
-            OPTIONAL MATCH (n)<-[:DAM]-(dam)
-            RETURN n, sire, dam;
-        """
-        result = tx.run(query)
-        try:
-            return [row for row in result]
-        # Capture any errors along with the query and data for traceability
-        except ServiceUnavailable as exception:
-            logging.error("{query} raised an error: \n {exception}".format(
-                query=query, exception=exception))
-            raise
-
-
-if __name__ == "__main__":
-    uri = os.environ.get('DATABASE_URL')
-    user = os.environ.get('DATABASE_USER')
-    password = os.environ.get('DATABASE_PASSWORD')
-    app = App(uri, user, password)
-    app.test()
-    app.close()
+# Evaluate the model
+y_pred = model.predict(X_test)
+accuracy = (y_pred == y_test).mean()
+print(f"Accuracy: {accuracy}")
